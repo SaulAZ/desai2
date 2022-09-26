c66cb59ccaa73c39c93dc8910a00f37b1de0a3c7
saulacorigmail.com <saulacorigmail.com>
2022-09-26 16:24:12 -0500

    Segunda Sesion:jdbc para mantenimiento y prepared statmente

--- src/main/java/com/isil/jdbc/MainApp.java
+++ src/main/java/com/isil/jdbc/MainApp.java
@@ -1,9 +1,7 @@
 package com.isil.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+
 
 public class MainApp {
 
@@ -21,11 +19,45 @@ public class MainApp {
         Statement st= con.createStatement();
 
         //ejecutar la sentencia SQL
-        ResultSet rs = st.executeQuery("select * from Users");
+        //consulta
+        //ResultSet rs = st.executeQuery("select * from Users");
 
-        //procesar el resultado
-        while(rs.next()){System.out.println(rs.getString("idUser") + " " + rs.getString("name") + " " + rs.getString("phone") + " " + rs.getString("city"));}
+        //Insertar
+        //int filasAfectadas= st.executeUpdate("insert into Users values(1,'Juan','987456123','Vancuver')");
+
+        //Actualizar
+        //int filasAfectadas=st.executeUpdate("update Users set name ='Christian' where name='Cueva'");
+
+        //Eliminar
+        //int filasAfectadas=st.executeUpdate("delete from Users where idUser=4");
+
+        //System.out.println("Filas Afectadas: "+filasAfectadas);
+
+        //Crear prepared statment
+        //prepared statement para mantenimiento
+        PreparedStatement preparedStatement = con.prepareStatement("insert into Users values(?,?,?,?)");
+preparedStatement.setInt(1, 8);
+preparedStatement.setString(2,"Marta");
+preparedStatement.setString(3,"985514581");
+preparedStatement.setString(4,"Bogota");
+
+int filasAfectadas= preparedStatement.executeUpdate();
+        System.out.println("Filas afectadas: "+filasAfectadas);
+
+    //prepared statement para consulta
+    PreparedStatement preparedStatement2= con.prepareStatement("SELECT * FROM USERS WHERE idUser=?");
+    preparedStatement2.setInt(1,5);
+
+    ResultSet rs= preparedStatement2.executeQuery();
+//procesar el resultado
+        while(rs.next()){
+            System.out.println(rs.getString("idUser") + " " +
+                    rs.getString("name") +
+                    " " + rs.getString("phone") +
+                    " " + rs.getString("city"));
         }
-
     }
+}
+
+
 
